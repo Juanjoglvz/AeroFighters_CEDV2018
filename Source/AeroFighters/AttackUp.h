133af8e0ff2cce03c562a27f14d5983c734aa566
Unknown <jcordobarom@gmail.com>
2018-05-10 22:46:54 +0200

    Added basic classes

---
+++ Source/AeroFighters/AttackUp.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Powerup.h"
+#include "AttackUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEROFIGHTERS_API AAttackUp : public APowerup
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/AeroFighters/BombUp.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Powerup.h"
+#include "BombUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEROFIGHTERS_API ABombUp : public APowerup
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
