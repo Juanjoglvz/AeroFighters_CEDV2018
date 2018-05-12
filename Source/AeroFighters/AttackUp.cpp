9a313ba17d663c9ec3b2340322f7ed84be257f92
Unknown <jcordobarom@gmail.com>
2018-05-12 19:56:52 +0200

    Powerup collisions. Not working

--- Source/AeroFighters/AttackUp.cpp
+++ Source/AeroFighters/AttackUp.cpp
@@ -3,5 +3,17 @@
 #include "AttackUp.h"
 
 
+AAttackUp::AAttackUp() : Super()
+{
+	PrimaryActorTick.bCanEverTick = true;
 
+	auto StaticMeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Assets/Asteroid/Asteroid_Round.Asteroid_Round'"));
 
+	if (StaticMeshAsset.Succeeded())
+		SetStaticMeshAsset(StaticMeshAsset.Object);
+}
+
+void AAttackUp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
