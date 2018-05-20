42acdf0a74d7cb344572a6fafe924bf7865fdd10
JUAN JOSE CORROTO MARTIN <JuanJose.Corroto@alu.uclm.es>
2018-05-20 12:45:08 +0200

    Added boss spawning and movement

--- Source/AeroFighters/Boss.cpp
+++ Source/AeroFighters/Boss.cpp
@@ -1,39 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Boss.h"
+#include "PlayerProjectile.h"
+#include "PlayerLaser.h"
+#include "PlayerMissile.h"
 
 
 // Sets default values
-ABoss::ABoss() : CameraSpeed {150.0f, 0.0f, 0.0f}
+ABoss::ABoss() : CameraSpeed {150.0f, 0.0f, 0.0f}, Hp{100}, LaserDmg{10}, MissileDmg{50}
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Create components
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
 
 	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
-	
 	auto BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
 
 	BoxCollision->SetCollisionProfileName(TEXT("OverlapAll"));
 	BoxCollision->SetupAttachment(RootComponent);
 	BoxCollision->bGenerateOverlapEvents = true;
-	//BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &APowerup::OnOverlap);
+	StaticMeshComponent->SetupAttachment(RootComponent);
 
-	auto StaticMeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Engine/BasicShapes/Cube.Cube'"));
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ABoss::OnOverlap);
+
+	auto StaticMeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Assets/Ships/Frigate'"));
 
 	if (StaticMeshAsset.Succeeded())
-		SetStaticMeshAsset(StaticMeshAsset.Object);
+	{
+		StaticMeshComponent->SetStaticMesh(StaticMeshAsset.Object);
+		StaticMeshComponent->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+		StaticMeshComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	}
 
-}
-
-void ABoss::SetStaticMeshAsset(UStaticMesh* StaticMeshAsset)
-{
-	StaticMeshComponent->SetStaticMesh(StaticMeshAsset);
-	StaticMeshComponent->SetMobility(EComponentMobility::Movable);
-	StaticMeshComponent->SetEnableGravity(false);
-	StaticMeshComponent->SetSimulatePhysics(false);
-	StaticMeshComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
 }
 
 // Called when the game starts or when spawned
@@ -49,11 +49,18 @@ void ABoss::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	FVector NewLocation = GetActorLocation();
-
 	NewLocation += CameraSpeed * DeltaTime;
 
+	if (IsValid(Position))
+	{
+		if (this->GetActorLocation().X > (this->Position->GetActorLocation().X + 550))
+		{
+			NewLocation.X -= 50 * DeltaTime;
+		}
+	}
+
+
 	SetActorLocation(NewLocation);
-	UE_LOG(LogTemp, Warning, TEXT("X: %f  Y: %f  Z: %f"), NewLocation.X, NewLocation.Y, NewLocation.Z);
 
 }
 
@@ -64,3 +71,34 @@ void ABoss::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
+void ABoss::SetPosition(AActor* Position)
+{
+	this->Position = Position;
+}
+
+
+void ABoss::OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		if (OtherActor->GetClass()->IsChildOf(APlayerProjectile::StaticClass()))
+		{
+			// Reduce health and destroy if less than 0
+			if (OtherActor->GetClass()->IsChildOf(APlayerLaser::StaticClass()))
+			{
+				this->Hp -= this->LaserDmg;
+			}
+			else if (OtherActor->GetClass()->IsChildOf(APlayerMissile::StaticClass()))
+			{
+				this->Hp -= this->MissileDmg;
+			}
+			if (this->Hp < 0)
+			{
+				this->Destroy();
+			}
+
+
+			OtherActor->Destroy();
+		}
+	}
+}
--- Source/AeroFighters/Boss.h
+++ Source/AeroFighters/Boss.h
@@ -20,6 +20,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+		void OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -30,10 +33,16 @@ public:
 	UPROPERTY()
 		UStaticMeshComponent* StaticMeshComponent;
 
+	UFUNCTION(BlueprintCallable)
+		void SetPosition(AActor* Position);
 	
 private:
-	void SetStaticMeshAsset(UStaticMesh* StaticMeshAsset);
 
 	FVector CameraSpeed;
-	
+	UPROPERTY()
+		AActor* Position;
+
+	float Hp;
+	float LaserDmg;
+	float MissileDmg;
 };
