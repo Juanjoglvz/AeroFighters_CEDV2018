6139953143d31a4e881f2bc7a612d29b3f6bcabe
ivangarrera <iesivangarcia@gmail.com>
2018-06-12 14:28:36 +0200

    First steps in movement replication

---
+++ Source/Alliance/MyCharacterMovement.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "MyCharacterMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALLIANCE_API UMyCharacterMovement : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
